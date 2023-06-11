220586273c80cd9b2a08c6f0bbab8e2559eb8dda
Nafie Asfour <ph.nafee.asfour@gmail.com>
2023-06-11 21:02:42 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Code your solution here
+
+function findMatching(names, searchName) {
+  const lowerSearchName = searchName.toLowerCase();
+
+  const matchingDrivers = names.filter(driver =>
+    driver.toLowerCase().includes(lowerSearchName)
+  );
+
+  return matchingDrivers;
+}
+function fuzzyMatch(names, prefix) {
+  const lowerPrefix = prefix.toLowerCase();
+  const matchingDrivers = names.filter(driver =>
+    driver.toLowerCase().startsWith(lowerPrefix)
+  );
+
+  return matchingDrivers;
+}
+function matchName(drivers, name) {
+  const matchingDrivers = drivers.filter(driver =>
+    driver.name === name
+  );
+
+  return matchingDrivers;
+}
+
+
+
+
+
+
+
+
